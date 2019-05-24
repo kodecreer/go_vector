a16f1f6a88b32910c594b571c30404dbdc66df05
Ni Nisan Nijackle <kode.creer@gmail.com>
2019-05-24 13:26:04 -0600

    changed vector prefix to GoVector

--- examples/main.go
+++ examples/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	vector "github.com/kodecreer/go_vector"
+	GoVector "github.com/kodecreer/go_vector"
 )
 
 type Player struct {
@@ -13,8 +13,8 @@ type Player struct {
 }
 
 func main() {
-	var players vector.Vector
-	playSlice := make([]vector.T, 20)
+	var players GoVector.Vector
+	playSlice := make([]GoVector.T, 20)
 	for i := 0; i < 20; i++ {
 		playerName := fmt.Sprintf("NoobSlayer %d", i)
 		player := Player{id: i, name: playerName, trophies: i * 100}
@@ -23,25 +23,31 @@ func main() {
 	//Or, if I want to initialize an empty  vector, I call Init() instead
 	players = players.InitWithSlice(playSlice)
 
+	//Oh no, I messed up with the first index
+	playerTypo := Player{id: 0, name: "King Gopher over 9000", trophies: 9001}
+	//nothing like a easy fix
+	players.SetAt(0, playerTypo)
+
+	//putting nil in the parameter invokes the default behavior for the map funciton
+	//which just returns copy of the vector
+	playerCopy := players.FpMap(nil)
+	fmt.Printf("Copy size is %d vs the original size of %d", playerCopy.Size(), players.Size())
+
 	//Declaring an inline function that act as a closure
-	players.FpMap(func(player vector.T) vector.T {
-		// fmt.Println(player.(Player).name)
-		//If I wanted to return a whole copy of an array, then I just return player and set it equal to map function
-		return nil
-	})
-	//Seperating players who have 1000+ trophies
-	noNoobs := players.FpFilter(func(player vector.T) bool {
-		// fmt.Println(player)
+	noNoobs := players.FpFilter(func(player GoVector.T) bool {
+		fmt.Println(player.(Player).name)
 		return player.(Player).trophies > 1000
 	})
-	noobTrophies := noNoobs.FpMap(func(player vector.T) vector.T {
-		//All I want is just the trophies from each player
+	noobTrophies := noNoobs.FpMap(func(player GoVector.T) GoVector.T {
+		//If I wanted to return a whole copy of an array, then I just return player and set it equal to map function
 		return player.(Player).trophies
 	})
-	proTrophieSum := noobTrophies.FpReduce(func(v1 vector.T, v2 vector.T) vector.T {
+	proTrophieSum := noobTrophies.FpReduce(func(v1 GoVector.T, v2 GoVector.T) GoVector.T {
 		return v1.(int) + v2.(int)
 	})
 
 	fmt.Println(proTrophieSum)
+	//Let's empty the vecotr
+	players.Clear()
 
 }
